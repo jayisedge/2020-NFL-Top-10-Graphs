25426275b053a5b818f930ded549248524cafb7b
Jeremy (Jay) Balestrieri <88201329+jayisedge@users.noreply.github.com>
2021-10-12 16:09:03 -0400

    Update Dropdowns.py

--- Tkinter-Version/Dropdowns.py
+++ Tkinter-Version/Dropdowns.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from tkinter import ttk
-from Master import passing, rushing, receiving
+from Master import top_ten
 
 cat=['Passing', 'Rushing', 'Receiving']
 
-pass_cat=['Pass Yards', 'Passing Touchdowns',  'Interceptions', 'Attempts', 'Completions', 'Yards Per Game', 'Sacks', 'Sack Yards Lost', '1st Downs', 
+pass_cat=['Passing Yards', 'Passing Touchdowns',  'Interceptions', 'Attempts', 'Completions', 'Yards Per Game', 'Sacks', 'Sack Yards Lost', '1st Downs', 
           'Longest Completion', '4th Quarter Comebacks', 'Game Winning Drives']
 
 rush_cat=['Rushing Yards', 'Rushing Touchdowns', 'Carries', 'Yards Per Carry', 'Yards Per Game', 'Fumbles', '1st Downs', 'Longest Rush']
@@ -12,21 +12,21 @@ rush_cat=['Rushing Yards', 'Rushing Touchdowns', 'Carries', 'Yards Per Carry', '
 rec_cat=['Receiving Yards', 'Receiving Touchdowns', 'Receptions', 'Targets', 'Yards Per Game', 'Receptions Per Game', 'Yards Per Reception', 'Yards Per Target',
          'Fumbles', '1st Downs', 'Longest Reception']
 
-def statistic(event):
-    if cat_drop.get()=='Passing':
-        stat_drop.config(value=pass_cat)
-    elif cat_drop.get()=='Rushing':
-        stat_drop.config(value=rush_cat)
+def populate_stats(event):
+    if categories.get()=='Passing':
+        statistics.config(value=pass_cat)
+    elif categories.get()=='Rushing':
+        statistics.config(value=rush_cat)
     else:
-        stat_drop.config(value=rec_cat)
+        statistics.config(value=rec_cat)
 
 def graph(event):
-    if cat_drop.get()=='Passing':
-        passing(stat_drop.get())
-    elif cat_drop.get()=='Rushing':
-        rushing(stat_drop.get())
+    if categories.get()=='Passing':
+        top_ten(statistics.get(), 'qb')
+    elif categories.get()=='Rushing':
+        top_ten(statistics.get(), 'rush')
     else:
-        receiving(stat_drop.get())
+        top_ten(statistics.get(), 'rec')
         
 def _quit():
     root.quit()
@@ -36,13 +36,13 @@ root=Tk()
 root.title('Graph 2020 NFL League Leaders')
 root.geometry('400x400')
 
-cat_drop=ttk.Combobox(root,value=cat)
-cat_drop.pack(pady=20)
-cat_drop.bind('<<ComboboxSelected>>', statistic)
+categories=ttk.Combobox(root,value=cat)
+categories.pack(pady=20)
+categories.bind('<<ComboboxSelected>>', populate_stats)
 
-stat_drop=ttk.Combobox(root,value=None)
-stat_drop.pack(pady=20)
-stat_drop.bind('<<ComboboxSelected>>', graph)
+statistics=ttk.Combobox(root,value=None)
+statistics.pack(pady=20)
+statistics.bind('<<ComboboxSelected>>', graph)
 
 graph_button=Button(root, text='Display Top 10 Graph', command=_quit)
 graph_button.pack(pady=20)
